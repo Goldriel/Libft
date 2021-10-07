91bdfc7a2ac3a3275d00ee0043f234376605b1c5
jarrakis <goldriel@mail.ru>
2021-10-08 01:09:31 +0300

    strlen ok

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,19 +6,18 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 00:39:10 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/10/08 00:43:51 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/10/08 00:54:37 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
-size_t  strlen(const char *c)
+size_t  ft_strlen(const char *c)
 {
     size_t  i;
 
-    i = 0
-    while (s[i] != '\0')
+    i = 0;
+    while (c[i] != '\0')
         i++;
     return (i);
 }
--- libft.h
+++ libft.h
@@ -6,18 +6,19 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:44:46 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/10/08 00:44:14 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/10/08 00:54:26 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stddef.h>
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
-size_t  strlen(const char *c);
+size_t  ft_strlen(const char *c);
 
 #endif
