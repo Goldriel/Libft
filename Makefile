99a33e7acebd58d722b13a4a2ba2b16699f7f3bd
Janette Arrakis <jarrakis@il-h5.msk.21-school.ru>
2021-10-12 17:30:38 +0300

    puc

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/08 18:58:28 by jarrakis          #+#    #+#              #
-#    Updated: 2021/10/11 20:15:24 by jarrakis         ###   ########.fr        #
+#    Updated: 2021/10/12 16:57:27 by jarrakis         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,7 +22,8 @@ LIST =		ft_isalnum.c\
 			ft_bzero.c\
 			ft_memcpy.c\
 			ft_memmove.c\
-			ft_strlcpy.c
+			ft_strlcpy.c\
+			ft_strlcat.c
 
 OBJ = $(patsubst %.c, %.o, $(LIST))
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:44:46 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/10/11 20:24:33 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/10/12 16:57:04 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
