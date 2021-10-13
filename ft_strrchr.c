1cc378063d0e9f95e827bbc781043c7f5e4da731
Janette Arrakis <jarrakis@il-h1.msk.21-school.ru>
2021-10-13 18:16:11 +0300

    strchr ok and strrchr create

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,28 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/12 19:51:31 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/10/13 18:11:55 by jarrakis         ###   ########.fr       */
+/*   Created: 2021/10/13 18:14:16 by jarrakis          #+#    #+#             */
+/*   Updated: 2021/10/13 18:15:32 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	char	*sh;
-	char	ch;
-
-	sh = (char *)s;
-	ch = (char)c;
-	while (*sh && *sh != ch)
-		(sh)++;
-	if (*sh == ch)
-		return (sh);
-	else
-		return (NULL);
 }
