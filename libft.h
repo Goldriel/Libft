0f0165fe6329a15aa4cd76771a5af6de43badcb1
Janette Arrakis <jarrakis@il-h1.msk.21-school.ru>
2021-10-13 21:02:24 +0300

    strncmp 404

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 15:44:46 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/10/13 20:11:55 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/10/13 20:35:53 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
