b36da446204f0d6bc0be1c77c5ad82d2d3b87bea
Janette Arrakis <jarrakis@il-a3.msk.21-school.ru>
2021-10-14 16:07:29 +0300

    norm ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 20:34:17 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/10/14 01:31:34 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/10/14 16:06:10 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    unsigned char	*str1;
+	unsigned char	*str1;
 	unsigned char	*str2;
-	size_t	i;
+	size_t			i;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
